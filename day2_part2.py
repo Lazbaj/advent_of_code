09b773b7169f2c81588b47775a73274176b4d511
Lazzaro Baj <6115949+Lazbaj@users.noreply.github.com>
2019-12-13 13:51:18 +0000

    Create day2_part2.py

---
+++ day2_part2.py
@@ -0,0 +1,58 @@
+memory = [
+    1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 6, 1, 19, 1, 5, 19, 23,
+    1, 13, 23, 27, 1, 6, 27, 31, 2, 31, 13, 35, 1, 9, 35, 39, 2, 39, 13, 43, 1,
+    43, 10, 47, 1, 47, 13, 51, 2, 13, 51, 55, 1, 55, 9, 59, 1, 59, 5, 63, 1, 6,
+    63, 67, 1, 13, 67, 71, 2, 71, 10, 75, 1, 6, 75, 79, 1, 79, 10, 83, 1, 5,
+    83, 87, 2, 10, 87, 91, 1, 6, 91, 95, 1, 9, 95, 99, 1, 99, 9, 103, 2, 103,
+    10, 107, 1, 5, 107, 111, 1, 9, 111, 115, 2, 13, 115, 119, 1, 119, 10, 123,
+    1, 123, 10, 127, 2, 127, 10, 131, 1, 5, 131, 135, 1, 10, 135, 139, 1, 139,
+    2, 143, 1, 6, 143, 0, 99, 2, 14, 0, 0
+]
+
+nouns = range(0, 100)  #[0..99]
+verbs = range(0, 100)  #[0..99]
+res = [0, 0]
+
+
+op_code = 0
+x = 0
+y = 0
+z = 0
+
+for noun in nouns:
+    if res != [0,0]:
+      break
+    
+    for verb in verbs:
+        print (noun, verb)
+        
+        input = memory.copy()
+        input[1] = noun
+        input[2] = verb
+        pointer = 0
+
+        while pointer < len(input):
+            if input[pointer] == 99:
+                print("CODE 99 FOUND - EXIT PROGRAM")
+                break
+            else:
+                op_code = input[pointer]
+                x = input[pointer + 1]
+                y = input[pointer + 2]
+                z = input[pointer + 3]
+                print("INSTRUCTION: (%d, %d, %d, %d)" % (op_code, x, y, z))
+
+                if op_code == 1:
+                    input[z] = input[x] + input[y]
+                elif op_code == 2:
+                    input[z] = input[x] * input[y]
+                else:
+                    print("ERROR: INVALID CODE")
+                    break
+                pointer += 4
+        if input[0] == 19690720:
+            res = [noun, verb]
+            break
+
+print(res)
+print(100*res[0]+res[1])
